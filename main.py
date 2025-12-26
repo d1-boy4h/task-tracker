c16c0c47bb3b919773748844ec85958127a6355f
d1_boy4h <d1.boy4h@gmail.com>
2025-12-26 16:35:26 +0300

    1.5.1: hotfix: убрал лишний код

--- main.py
+++ main.py
@@ -8,10 +8,6 @@ Task Tracker - консольное приложение для управлен
 
 from src.core.tracker import TaskTracker
 
-__version__ = '1.5'
-__author__ = 'd1_boy4h'
-PROGRAM_NAME = 'Task Tracker'
-
 if __name__ == '__main__':
     tracker = TaskTracker()
     tracker.run()
