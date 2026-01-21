367f9a5bff276eb63a91af47d293504a019937e7
d1_boy4h <d1.boy4h@gmail.com>
2026-01-21 05:01:54 +0300

    1.5.4a: убрал ненужный super() в NoNameTaskException

--- src/constants/constants.py
+++ src/constants/constants.py
@@ -2,5 +2,5 @@
 
 PROGRAM_NAME = 'Task Tracker'
 
-__version__ = '1.5.4'
+__version__ = '1.5.4a'
 __author__ = 'd1_boy4h'
--- src/models/exceptions.py
+++ src/models/exceptions.py
@@ -3,5 +3,4 @@
 class NoNameTaskException(Exception):
     '''Исключение создания задачи без имени'''
     def __init__(self):
-        super()
         self.msg = 'задача не может быть пустой!'
